499fcf461fe141916701e2143deea1d217c5bcb5
311 <311@SALN311>
2022-05-20 20:42:44 -0500

    Agregar archivos de proyecto.

---
+++ CinemaApp.Infrastructure/Migrations/20220521001255_InsercionSalas.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CinemaApp.Infrastructure.Migrations
+{
+    public partial class InsercionSalas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Sala",
+                columns: new[] { "IdSala", "Activa", "Capacidad", "EsDinamix", "Nomenclatura" },
+                values: new object[,]
+                {
+                    { 1, true, 20, false, "S1" },
+                    { 2, true, 30, true, "S2" },
+                    { 3, false, 50, false, "S3" },
+                    { 4, true, 40, true, "S4" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Sala",
+                keyColumn: "IdSala",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Sala",
+                keyColumn: "IdSala",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Sala",
+                keyColumn: "IdSala",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Sala",
+                keyColumn: "IdSala",
+                keyValue: 4);
+        }
+    }
+}
